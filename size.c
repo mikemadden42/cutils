3851d87b07704f9a42cd052fc8f7297e7244ca98
Michael Madden <mikemadden42@icloud.com>
2017-03-17 12:00:02 -0500

    Clean up warnings from MS VS 2015.

--- size.c
+++ size.c
@@ -1,9 +1,10 @@
 // http://stackoverflow.com/questions/3209909/how-to-printf-unsigned-long-in-c
+// http://stackoverflow.com/questions/1546789/clean-code-to-printf-size-t-in-c-or-nearest-equivalent-of-c99s-z-in-c
 
 #include <stddef.h>
 #include <stdio.h>
 
-int main() {
-  printf("sizeof(size_t) = %lu\n", sizeof(size_t));
+int main(void) {
+  printf("sizeof(size_t) = %zu\n", sizeof(size_t));
   return 0;
 }
